fd09d2eaa5f492d32e6213ae0e68e0d74cf667e1
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-01-29 03:05:45 +0900

    Refactoring of To Dos App

--- src/App.js
+++ src/App.js
@@ -1,62 +1,65 @@
-import React, { useState, useReducer } from "react";
-import { v4 as uuid } from "uuid";
-
-const initialState = { toDos: [] };
-const ADD = "add";
-const DEL = "del";
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case ADD:
-      return { toDos: [...state.toDos, { text: action.payload, id: uuid() }] };
-    case DEL:
-      return {
-        toDos: state.toDos.filter((toDo) => toDo.id !== action.payload),
-      };
-    default:
-      throw new Error();
-  }
-};
+import React from "react";
+import Add from "./Add";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const [newToDo, setNewToDo] = useState("");
-  const onSubmit = (e) => {
-    e.preventDefault();
-    dispatch({ type: ADD, payload: newToDo });
-    setNewToDo("");
-  };
-  const onChange = (e) => {
-    const {
-      target: { value },
-    } = e;
-    setNewToDo(value);
-  };
   return (
     <>
-      <h1>Add to do</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={newToDo}
-          type="text"
-          placeholder="Write to do"
-          onChange={onChange}
-        />
-      </form>
+      <Add />
       <ul>
         <h2>To Dos</h2>
         {state.toDos.map((toDo) => (
           <li key={toDo.id}>
             <span>{toDo.text}</span>
-            <button
+            <span
+              role="img"
+              aria-label=""
               onClick={() => {
                 dispatch({ type: DEL, payload: toDo.id });
               }}
             >
               ❌
-            </button>
+            </span>
+            <span
+              role="img"
+              aria-label=""
+              onClick={() => {
+                dispatch({ type: COMPLETE, payload: toDo.id });
+              }}
+            >
+              ✅
+            </span>
           </li>
         ))}
+      </ul>
+      <ul>
+        {state.completed.length !== 0 && (
+          <>
+            <h2>Completed</h2>
+            {state.completed.map((toDo) => (
+              <li key={toDo.id}>
+                <span>{toDo.text}</span>
+                <span
+                  role="img"
+                  aria-label=""
+                  onClick={() => {
+                    dispatch({ type: DEL, payload: toDo.id });
+                  }}
+                >
+                  ❌
+                </span>
+                <span
+                  role="img"
+                  aria-label=""
+                  onClick={() => {
+                    dispatch({ type: UNCOMPLETE, payload: toDo.id });
+                  }}
+                >
+                  🙅‍♂️
+                </span>
+              </li>
+            ))}
+          </>
+        )}
       </ul>
     </>
   );
